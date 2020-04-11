ef8106044538b6a2380426fa80693027f3989aaa
JmilkFan <fangui_ju@163.com>
2020-04-11 22:28:09 +0800

    Rename to lispy.c

--- lispy.c
+++ lispy.c
@@ -1141,7 +1141,7 @@ int main(int argc, char *argv[]) {
 
     /* Interactive Prompt */
     if (argc == 1) {
-        puts("Lispy Version 0.1");
+        puts("Lispy Version 1.0");
         puts("Press Ctrl+c to Exit\n");
 
         while(1) {
